a924daadb5b67737f16c66912134433011d6b4fd
Dawid 'Sloenthran' Nogacz <sloenthran@gmail.com>
2020-04-10 17:06:54 +0200

    Init commit

---
+++ src/main/java/pl/nogacz/allegro/AllegroApplication.java
@@ -0,0 +1,13 @@
+package pl.nogacz.allegro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllegroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllegroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/nogacz/allegro/AllegroApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.nogacz.allegro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllegroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
